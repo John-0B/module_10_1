c1d2e68cbf724ebc8ee63a4d000d1dca57a1fa02
John-0B <ivbg@yandex.ru>
2024-09-23 15:08:29 +0300

    Add files via upload

---
+++ module_10_1.py
@@ -0,0 +1,39 @@
+from threading import Thread
+from datetime import datetime
+from time import sleep
+
+
+def write_words(word_count, file_name):
+    with open(file_name, 'a', encoding='utf-8') as file:
+        for i in range(word_count):
+            file.write(f'Какое-то слово № {i+1}\n')
+            sleep(0.1)
+        print(f'Завершилась запись в файл {file_name}')
+
+
+start_t = datetime.now()
+write_words(10, 'example1.txt')
+write_words(30, 'example2.txt')
+write_words(200, 'example3.txt')
+write_words(100, 'example4.txt')
+stop_t = datetime.now()
+print(f'Работа функции = {stop_t - start_t}')
+
+start_t = datetime.now()
+thr_first = Thread(target=write_words, args=(10, 'example5.txt'))
+thr_second = Thread(target=write_words, args=(30, 'example6.txt'))
+thr_third = Thread(target=write_words, args=(200, 'example7.txt'))
+thr_fourth = Thread(target=write_words, args=(100, 'example8.txt'))
+
+thr_first.start()
+thr_second.start()
+thr_third.start()
+thr_fourth.start()
+
+thr_first.join()
+thr_second.join()
+thr_third.join()
+thr_fourth.join()
+
+stop_t = datetime.now()
+print(f'Работа потоков = {stop_t - start_t}')
